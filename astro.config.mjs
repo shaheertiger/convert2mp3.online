d1993392fc6e5494347108d62a2a98ba814a247a
shaheertiger <77096267+shaheertiger@users.noreply.github.com>
2026-01-06 00:06:33 -0500

    Add sitemap generation support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://convert2mp3.online',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   i18n: {
     defaultLocale: 'en',
